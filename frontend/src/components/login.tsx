eb16b96db80baabd25d702dda8b7cdd31da5d1cf
ivanoung <ivanoung@gmail.com>
2018-05-30 23:18:59 +0800

    styling first page

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -4,11 +4,13 @@ export default class Login extends React.Component {
   public render() {
     return (
       <div className="login-grid">
-        <input type="text" id="id"/>
-        <input type="password" id="password"/>
-        <button type="submit" id="submit">S</button>
-        <button id="google">Google</button>
-        <button id="facebook">Facebook</button>
+        <input type="text" id="id" />
+        <input type="password" id="password" />
+        <div id="submit">
+          S
+        </div>
+        <div id="google">Google</div>
+        <div id="facebook">Facebook</div>
       </div>
     );
   }
