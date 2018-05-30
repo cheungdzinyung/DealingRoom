5d19ddfcac71f25b25bdbfcb65a932d619f2e73e
ivanoung <ivanoung@gmail.com>
2018-05-30 22:49:43 +0800

    loaded SCSS and working on homepage

---
+++ frontend/src/components/login.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export default class Login extends React.Component {
+  public render() {
+    return (
+      <div className="login-grid">
+        <input type="text" />
+        <input type="password" />
+      </div>
+    );
+  }
+}
