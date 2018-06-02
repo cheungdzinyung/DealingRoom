82f34a486f548538b70d8713f953c9201bd11ce2
ivanoung <ivanoung@gmail.com>
2018-06-02 14:49:18 +0800

    payment image upload

--- frontend/src/components/payment.tsx
+++ frontend/src/components/payment.tsx
@@ -2,15 +2,23 @@ import * as React from "react";
 import Usermenu from "../components/usermenu";
 import Banner from './topbanner'
 
-export default class Profile extends React.Component<{ header: string }> {
-    constructor(props: { header: string }) {
+import Paypal from '../images/payment/stripe.png'
+
+interface IProfileProps {
+    paymentMethods: string[];
+}
+
+export default class Profile extends React.Component<IProfileProps> {
+    constructor(props: IProfileProps) {
         super(props);
+
+        this.state = {  }
     }
     public render() {
         return (
-
             <div className="userInterface">
                 <Banner header="Payment" />
+                <img src={Paypal} alt=""/>
                 <Usermenu />
             </div>
         );
