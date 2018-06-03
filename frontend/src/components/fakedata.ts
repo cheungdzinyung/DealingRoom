de8c1febc050e04e40807c95cc01292ffd6f9ebf
ivanoung <ivanoung@gmail.com>
2018-06-03 22:01:11 +0800

    adding menu section

--- frontend/src/components/fakedata.ts
+++ frontend/src/components/fakedata.ts
@@ -2,7 +2,8 @@ export const items = [
   {
     currentPrice: 60,
     description:
-      "Made with vodka, tequila, light rum, triple sec, gin, and a splash of cola, , which gives the drink the same amber hue as its namesake.",
+      "Made with vodka, tequila, light rum, triple sec, gin, and a splash of cola, , which gives the drink the same amber hue as its namesake."
+      ,
     name: "Asahi",
     percentage: -10
   },
--- frontend/src/components/pages/menu.tsx
+++ frontend/src/components/pages/menu.tsx
@@ -1,4 +1,4 @@
-import { Card, Elevation } from "@blueprintjs/core";
+import { Card, Collapse, Elevation } from "@blueprintjs/core";
 import * as React from "react";
 import Usermenu from "../share/usermenu";
 
@@ -13,6 +13,7 @@ interface IMenuItem {
 
 interface IMenuState {
   items: IMenuItem[];
+  isOpen: boolean;
 }
 
 export default class Menu extends React.Component<{}, IMenuState> {
@@ -20,19 +21,42 @@ export default class Menu extends React.Component<{}, IMenuState> {
     super(props);
 
     this.state = {
+      isOpen: false,
       items
     };
   }
+
+  public onOff = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
   public render() {
     return (
       <div className="page-content-container">
-        {this.state.items.map(itm => (
-          <Card interactive={true} elevation={Elevation.FOUR}>
-            <h2>{itm.name}</h2>
-            <p>${itm.currentPrice}</p>
-            <p>{itm.percentage}%</p>
-            <p>{itm.description}</p>
-          </Card>
+        <input
+          className="pt-input searchbar"
+          type="search"
+          placeholder="Search input"
+          dir="auto"
+        />
+        {this.state.items.map((item, i) => (
+          <div className="item-container">
+            <Card
+              className="item-cards item-price-up"
+              interactive={true}
+              elevation={Elevation.FOUR}
+              onClick={this.onOff}
+            >
+              <span>{item.name}</span>
+              <span>${item.currentPrice}</span>
+              <span>{item.percentage}%</span>
+            </Card>
+            <Collapse className={"item-details"+ " " + (this.state.isOpen ? "item-detail-onflex" : '')} isOpen={this.state.isOpen}>
+              <span>{item.description}</span>
+            </Collapse>
+          </div>
         ))}
         <Usermenu />
       </div>
