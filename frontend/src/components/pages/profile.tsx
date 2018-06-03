837aa8e89f8dfdffd5f9ade0abf6dc3f51f85f0d
ivanoung <ivanoung@gmail.com>
2018-06-03 10:42:53 +0800

    graph fixing

--- frontend/src/components/pages/profile.tsx
+++ frontend/src/components/pages/profile.tsx
@@ -1,18 +1,76 @@
 import * as React from "react";
-import Banner from '../share/topbanner'
+import Banner from "../share/topbanner";
 import Usermenu from "../share/usermenu";
 
-import tempImg from '../../images/circle-head.png';
+import { Bar } from "react-chartjs-2";
 
-export default class Profile extends React.Component<{ header: string }> {
-  constructor(props:{header: string}){
+import { Card, Elevation } from "@blueprintjs/core";
+import tempImg from "../../images/circle-head.png";
+
+interface IProfileProps {
+  header: string;
+}
+
+interface IProfileState {
+  dataaa: any;
+}
+
+export default class Profile extends React.Component<
+  IProfileProps,
+  IProfileState
+> {
+  constructor(props: IProfileProps) {
     super(props);
+
+    this.state = {
+      dataaa: {
+        labels: [
+          "Eating",
+          "Drinking",
+          "Sleeping",
+          "Designing",
+          "Coding",
+          "Cycling",
+          "Running"
+        ],
+        // tslint:disable-next-line:object-literal-sort-keys
+        datasets: [
+          {
+            label: "My First dataset",
+            // tslint:disable-next-line:object-literal-sort-keys
+            fillColor: "rgba(220,220,220,1)",
+            strokeColor: "rgba(220,220,220,1)",
+            // tslint:disable-next-line:object-literal-sort-keys
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 90, 81, 56, 55, 0]
+          },
+          {
+            label: "My Second dataset",
+            // tslint:disable-next-line:object-literal-sort-keys
+            fillColor: "rgba(151,187,205,1)",
+            strokeColor: "rgba(151,187,205,1)",
+            // tslint:disable-next-line:object-literal-sort-keys
+            pointColor: "rgba(234,60,90,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 96, 27, 100]
+          }
+        ]
+      }
+    };
   }
   public render() {
     return (
-
       <div className="userInterface">
-        <Banner header="Profile" image={tempImg}/>
+        <Banner header="Profile" image={tempImg} />
+        <Bar data={this.state.dataaa} />
+        <Card interactive={true} elevation={Elevation.FOUR}>
+          <Bar data={this.state.dataaa} />
+        </Card>
         <Usermenu />
       </div>
     );
