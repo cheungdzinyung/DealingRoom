2ab59ac99a3842ad3101dd777c12934e368deb9c
ivanoung <ivanoung@gmail.com>
2018-06-04 10:03:47 +0800

    reorganize scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Route, Switch } from "react-router";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./scss/App.scss";
 
 import Login from "./components/pages/login";
@@ -13,19 +12,19 @@ import Setting from "./components/pages/setting";
 class App extends React.Component {
   public render() {
     return (
-      <Router>
-        <div className="full-page">
-          {/* TODO: To move each page container into common space */}
-          <Switch>
-            <Route exact={true} path="/" component={Login} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/order" component={Order} />
-            <Route path="/menu" component={Menu} />
-            <Route path="/payment" component={Payment} />
-            <Route path="/setting" component={Setting} />
-          </Switch>
-        </div>
-      </Router>
+
+      <div className="full-page">
+        {/* TODO: To move each page container into common space */}
+        <Switch>
+          <Route exact={true} path="/" component={Login} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/order" component={Order} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/payment" component={Payment} />
+          <Route path="/setting" component={Setting} />
+        </Switch>
+      </div>
+
     );
   }
 }
