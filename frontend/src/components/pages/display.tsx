99b5c279d2ede51b7de1301888af205459e001bb
judith curtit <judithcurtit8@gmail.com>
2018-06-05 18:20:58 +0800

    display screen in progress

--- frontend/src/components/pages/display.tsx
+++ frontend/src/components/pages/display.tsx
@@ -4,6 +4,9 @@ import tempImg from "../../images/circle-head.png";
 
 import { categories, items } from "../fakedata";
 
+import down from "../../icons/down.svg";
+import up from "../../icons/up.svg";
+
 interface IMenuItem {
     name: string;
     percentage: number;
@@ -17,7 +20,7 @@ interface IMenuCategory {
 interface IDisplayState {
     items : IMenuItem[];
     categories : IMenuCategory;
-
+    categoryName: string;
   }
 
 export default class Display extends React.Component<{}, IDisplayState> {
@@ -27,24 +30,47 @@ export default class Display extends React.Component<{}, IDisplayState> {
       this.state = {
           // tslint:disable-next-line:object-literal-shorthand
           categories: categories,
+
+          categoryName: '',
           // tslint:disable-next-line:object-literal-shorthand
           items: items
       }
     }
 
+    // public componentWillMount(){} // use it for calling backend
+
+    public componentDidMount() {
+        // the category name should be either from previous page or from the backend
+        const categoryName = this.state.categories.name.find((name) => name === 'cocktail');
+        if (categoryName) {
+            this.setState({categoryName});
+        }
+    }
+
     public render() {
-        // const itemName = this.state.items;
-        // const itemCat = this.state.categories;
-        // const nameDisplay = itemName.map(name)
+       
         return (
-            <div className="display-container">
+            <div className="display-content-container">
                 <div className="display-banner">
                     <img className="display-img" src={tempImg} alt="display-pic" />
-                    <h2>${categories.name}</h2>
+                    <h2>{this.state.categoryName}</h2>
                 </div>
-                <div className="display-grid">
-                    <div className="item-name">{this.state.items[0].name}</div>
-                    <div className="item-percentage">{this.state.items[0].description}</div>
+                {/* style={{display: 'flex, flexWrap: 'wrap}} */}
+                <div className="display-container">
+                {this.state.items.map((item, i) => {
+                        return (
+                            // style={{ width: '400px', height: '150px' }}
+                            <div className="display-grid" key={i}>
+                                <div className="item-name-display">
+                                    <span>{item.name}</span>
+                                </div>
+                                <div className="item-arrow-display">
+                                    <img className='arrow-img' src={item.percentage > 0 ? up : down} alt="" />
+                                </div>
+                            </div>
+                        )
+                    }
+                )}
                 </div>
             </div>
               );
