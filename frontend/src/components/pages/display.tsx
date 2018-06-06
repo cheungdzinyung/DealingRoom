a885e05ef68d54464b2d8d0f0920bc15b7f13cc8
judith curtit <judithcurtit8@gmail.com>
2018-06-06 17:42:55 +0800

    still dispaly page!

--- frontend/src/components/pages/display.tsx
+++ frontend/src/components/pages/display.tsx
@@ -4,8 +4,8 @@ import tempImg from "../../images/circle-head.png";
 
 import { categories, items } from "../fakedata";
 
-import down from "../../icons/down.svg";
-import up from "../../icons/up.svg";
+import downWhite from "../../icons/down-white.svg";
+import upWhite from "../../icons/up-white.svg";
 
 interface IMenuItem {
     name: string;
@@ -41,7 +41,7 @@ export default class Display extends React.Component<{}, IDisplayState> {
 
     public componentDidMount() {
         // the category name should be either from previous page or from the backend
-        const categoryName = this.state.categories.name.find((name) => name === 'cocktail');
+        const categoryName = this.state.categories.name.find((name) => name === 'Cocktails');
         if (categoryName) {
             this.setState({categoryName});
         }
@@ -55,23 +55,28 @@ export default class Display extends React.Component<{}, IDisplayState> {
                     <img className="display-img" src={tempImg} alt="display-pic" />
                     <h2>{this.state.categoryName}</h2>
                 </div>
-                {/* style={{display: 'flex, flexWrap: 'wrap}} */}
                 <div className="display-container">
                 {this.state.items.map((item, i) => {
                         return (
-                            // style={{ width: '400px', height: '150px' }}
                             <div className="display-grid" key={i}>
                                 <div className="item-name-display">
                                     <span>{item.name}</span>
                                 </div>
-                                <div className="item-arrow-display">
-                                    <img className='arrow-img' src={item.percentage > 0 ? up : down} alt="" />
+                                <div className={
+                                    this.state.items.map[i]
+                                        ? "item-arrow-display"
+                                        : item.percentage > 0
+                                            ? "item-arrow-display item-price-up"
+                                            : "item-arrow-display item-price-down"
+                                }>
+                                    <img className='arrow' src={item.percentage > 0 ? upWhite : downWhite} alt="" />
                                 </div>
                             </div>
                         )
                     }
                 )}
-                </div>
+            </div>
+                <div className="rssFeed">This round of discount is brought to you by...</div>
             </div>
               );
     }
