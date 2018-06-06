a83c5d1b3429a6707ab8e3a07a413ae67747c9e8
Andrew Cheung <cheungdzinyung@gmail.com>
2018-06-06 11:09:41 +0800

    changed migration file to typescript

---
+++ backend/migrations/20180606104747_initial.ts
@@ -0,0 +1,142 @@
+import * as Knex from "knex";
+
+exports.up = function (knex: Knex) {
+    return knex.schema
+    .createTable("users", users => {
+      users
+        .increments("id")
+        .unsigned()
+        .primary();
+      users.string("username");
+      users.text("password");
+      users.string("displayName");
+      users.text("userPhoto").defaultTo("");
+      users.text("facebookToken");
+      users.enum("role", ["bartender", "server", "manager", "customer"]).notNullable();
+      users.boolean("isActive").defaultTo(true).notNullable();
+    })
+    .then(() => {
+      return knex.schema.createTable("orders", orders => {
+        orders
+          .increments("id")
+          .unsigned()
+          .primary();
+        orders
+          .integer("users_id")
+          .unsigned()
+          .notNullable();
+        orders.foreign("users_id").references("users.id");
+        orders.integer("table");
+        orders.enum("status", [
+          "ordered",
+          "confirmed",
+          "made",
+          "served",
+          "cancelled"
+        ]);
+        orders.boolean("isPaid").defaultTo(false);
+        orders.timestamps(false, true);
+      });
+    })
+    .then(() => {
+      return knex.schema.createTable("categories", categories => {
+        categories
+          .increments()
+          .unsigned()
+          .primary();
+        categories.enum("categoryName", [
+          "beer",
+          "cocktail",
+          "redWine",
+          "whiteWine",
+          "champagne",
+          "vodka",
+          "tequila",
+          "whiskey",
+          "gin",
+          "rum",
+          "brandy",
+          "non-alcoholic",
+          "snack",
+          "main",
+          "dessert"
+        ]);
+        categories.text("categoryPhoto");
+        categories.boolean("isActive").defaultTo(true).notNullable();
+      });
+    })
+    .then(() => {
+      return knex.schema.createTable("items", items => {
+        items
+          .increments()
+          .unsigned()
+          .primary();
+        items.string("itemName");
+        items.integer("itemStock").notNullable();
+        items
+          .integer("categories_id")
+          .unsigned()
+          .notNullable();
+        items.foreign("categories_id").references("categories.id");
+        items.decimal("minimumPrice");
+        items.decimal("currentPrice");
+        items.text("itemPhoto").defaultTo("");
+        items.text("itemDescription").defaultTo("");
+        items.boolean("isSpecial").defaultTo(false).notNullable();
+        items.boolean("isActive").defaultTo(true).notNullable();
+      });
+    })
+    .then(() => {
+      return knex.schema.createTable("orders_items", ordersItems => {
+        ordersItems
+          .increments()
+          .unsigned()
+          .primary();
+        ordersItems
+          .integer("orders_id")
+          .unsigned()
+          .notNullable();
+        ordersItems.foreign("orders_id").references("orders.id");
+        ordersItems
+          .integer("items_id")
+          .unsigned()
+          .notNullable();
+        ordersItems.foreign("items_id").references("items.id");
+        ordersItems.decimal("purchasePrice")
+        ordersItems
+          .enum("ice", ["extra", "normal", "without"])
+          .defaultTo("normal");
+        ordersItems
+          .enum("sweetness", ["extra", "normal", "without"])
+          .defaultTo("normal");
+        ordersItems
+          .enum("garnish", ["extra", "normal", "without"])
+          .defaultTo("normal");
+      });
+    })
+    .then(() => {
+      return knex.schema.createTable("itemsLog", itemsLog => {
+        itemsLog
+          .increments()
+          .unsigned()
+          .primary();
+        itemsLog
+          .integer("items_id")
+          .unsigned()
+          .notNullable();
+        itemsLog.foreign("items_id").references("items.id");
+        itemsLog.timestamps(false, true);
+        itemsLog.decimal("itemsLogPrice");
+      });
+    });
+};
+
+exports.down = function (knex: Knex) {
+    return knex.schema
+    .dropTable("itemsLog")
+    .then(() => knex.schema.dropTable("orders_items"))
+    .then(() => knex.schema.dropTable("items"))
+    .then(() => knex.schema.dropTable("categories"))
+    .then(() => knex.schema.dropTable("orders"))
+    .then(() => knex.schema.dropTable("users"));
+};
