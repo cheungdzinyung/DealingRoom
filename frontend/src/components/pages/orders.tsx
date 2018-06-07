f135794bd43f91fbefbcc357c46cf81c706e81ba
ivanoung <ivanoung@gmail.com>
2018-06-07 17:49:59 +0800

    orders page done as well

--- frontend/src/components/pages/orders.tsx
+++ frontend/src/components/pages/orders.tsx
@@ -1,8 +1,13 @@
+import { Card, Elevation } from "@blueprintjs/core";
 import * as React from "react";
 import OrderBanner from "../share/orderBanner";
 import Usermenu from "../share/usermenu";
 
+
+import checkIcon from "../../icons/check.svg"
 import headerImg from "../../icons/orders.svg";
+
+import { orderList } from "../fakedata";
 
 interface IOrder {
   orderNumber: number
@@ -11,35 +16,51 @@ interface IOrder {
   isPaid: boolean
 }
 
-interface IOrdersProps{
+interface IOrdersProps {
+  listOfOrder: IOrder[]
+}
+
+interface IOrdersStates {
   listOfOrder: IOrder[]
 }
 
 
-export default class Orders extends React.Component<IOrdersProps> {
+export default class Orders extends React.Component<IOrdersProps, IOrdersStates> {
   constructor(props: IOrdersProps) {
     super(props);
+
+    this.state = {
+      listOfOrder: orderList.listOfOrder
+    }
   }
   public render() {
     return (
       <div className="page-content-container">
         <OrderBanner displayName="Ivan" tableNumber={3} image={headerImg} status="Order" statusNumber={1326} />
 
-        <div className="cards">
-          <div className="top">
-            <div className="order-details">
-              <h1>
-                Order #{1234}
-              </h1>
-              <p>Total Amount: ${140.29}</p>
-              <p>Ordering time: {1530}</p>
-            </div>
-            <img src="" className="order-icon" alt="" />
-          </div>
+        {
+          this.state.listOfOrder.map((indOrd, index) => (<Card className="order-cards" interactive={true} elevation={Elevation.TWO}>
+            <div className="top">
+              <div className="order-details">
+                <h3>
+                  Order #{indOrd.orderNumber}
+                </h3>
+                <p>Total Amount: ${indOrd.amount}</p>
+                <p>Ordering time: {indOrd.orderTime}</p>
+              </div>
+              {indOrd.isPaid ? <img src={checkIcon} className="order-icon" alt="" /> : <br />}
 
-          <hr />
-            
-        </div>
+            </div>
+            <hr />
+            {indOrd.isPaid ? <br /> :
+              <button className="paynow-button"><span>Pay Now</span></button>
+            }
+
+          </Card>))
+        }
+
+
+
         <Usermenu />
       </div>
     );
