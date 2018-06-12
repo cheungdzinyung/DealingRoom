eb9c4ce96cb62f3077d175ad7f1723db929d85ea
ivanoung <ivanoung@gmail.com>
2018-06-12 15:10:03 +0800

    holy shit ton of changes

--- frontend/src/components/pages/orderlist.tsx
+++ frontend/src/components/pages/orderlist.tsx
@@ -1,6 +1,8 @@
 // Importing modules
 import { Card, Elevation } from "@blueprintjs/core";
+import * as History from "history";
 import * as React from "react";
+import { IPureUsersOrderList } from "../../modules"
 
 // Importing components
 import OrderBanner from "../share/orderbanner";
@@ -13,23 +15,21 @@ import headerImg from "../../icons/orders.svg";
 // Importing fake data
 import { orderList } from "../fakedata";
 
-interface IPureOrder {
-  orderNumber: number;
-  amount: number;
-  orderTime: number;
-  isPaid: boolean;
+
+interface IPureOrdersProps {
+  history: History.History
 }
 
 interface IPureOrdersStates {
-  listOfOrder: IPureOrder[];
+  userOrders: IPureUsersOrderList; 
 }
 
-export default class Orders extends React.Component<{}, IPureOrdersStates> {
-  constructor(props: {}) {
+export default class OrderList extends React.Component<IPureOrdersProps, IPureOrdersStates> {
+  constructor(props: IPureOrdersProps) {
     super(props);
 
     this.state = {
-      listOfOrder: []
+      listOfOrder:  
     };
   }
 
@@ -38,12 +38,15 @@ export default class Orders extends React.Component<{}, IPureOrdersStates> {
       listOfOrder: orderList.listOfOrder
     })
   }
+  public openSingleOrder = (orderNumber: number) => {
+    this.props.history.push(`/order/${orderNumber}`);
+  }
 
   public render() {
     return (
+      
       <div className="page-content-container">
         <OrderBanner displayName="Ivan" tableNumber={3} image={headerImg} />
-        {/* Dynamic content, TODO: refactorize it */}
         {this.state.listOfOrder.filter(each => !each.isPaid).length > 0 && (
           <div className="order-header-container">
             <h3 className="order-header">To be paid</h3>
@@ -56,6 +59,7 @@ export default class Orders extends React.Component<{}, IPureOrdersStates> {
               className="order-cards"
               interactive={true}
               elevation={Elevation.TWO}
+              onClick={this.openSingleOrder.bind(this, indOrd.orderNumber)}
             >
               <div className="top">
                 <div className="order-details">
@@ -75,10 +79,10 @@ export default class Orders extends React.Component<{}, IPureOrdersStates> {
         {/* Split into two parts */}
         {this.state.listOfOrder.filter(each => each.isPaid === true).length >
           0 && (
-          <div className="order-header-container">
-            <h3 className="order-header">Paid</h3>
-          </div>
-        )}
+            <div className="order-header-container">
+              <h3 className="order-header">Paid</h3>
+            </div>
+          )}
         {this.state.listOfOrder
           .filter(each => each.isPaid === true)
           .map((indOrd, index) => (
