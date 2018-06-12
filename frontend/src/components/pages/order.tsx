553d78997e21c34631710a68bf4dff5ff0a88e01
ivanoung <ivanoung@gmail.com>
2018-06-12 22:42:04 +0800

    mobile view 80ish% done, need to fine tune color, location and ahhhhhhhh

--- frontend/src/components/pages/order.tsx
+++ frontend/src/components/pages/order.tsx
@@ -1,8 +1,10 @@
 // Importing modules
-// import { Card, Elevation } from "@blueprintjs/core";
 import * as History from "history";
 import * as React from "react";
-import { IPureOrder } from "../../modules"
+import { match } from "react-router-dom";
+
+// Importing UI elements
+import { Card, Elevation } from "@blueprintjs/core";
 
 // Importing components
 import OrderBanner from "../share/orderbanner";
@@ -11,34 +13,65 @@ import Usermenu from "../share/usermenu";
 // Importing static assets
 // import checkIcon from "../../icons/check.svg";
 import headerImg from "../../icons/orders.svg";
+import paymentTest from "../../images/payment/stripe.png"
+
+// Importing types
+import { IPureOrder } from "../../modules";
 
 // Importing fake data
 import { singleOrder } from "../../fakedata";
 
 interface IPureOrderProps {
-    history: History.History;
+  history: History.History;
+  match: match<{ orderId: number }>;
 }
 
 interface IPureOrdersStates {
-    order: IPureOrder
+  order: IPureOrder;
 }
 
-export default class Order extends React.Component<IPureOrderProps, IPureOrdersStates> {
-    constructor(props: IPureOrderProps) {
-        super(props);
+export default class Order extends React.Component<
+  IPureOrderProps,
+  IPureOrdersStates
+> {
+  constructor(props: IPureOrderProps) {
+    super(props);
 
-        this.state = {
-            order: singleOrder
-        };
+    this.state = {
+      order: singleOrder
+    };
+  }
+  public render() {
+    return (
+      <div className="page-content-container">
+        <OrderBanner
+          displayName={this.state.order.displayName}
+          tableNumber={this.state.order.table}
+          image={headerImg}
+          status="Order"
+          statusNumber={this.props.match.params.orderId}
+        />
 
-    }
-    public render() {
-        return (
-            <div className="page-content-container">
-                <OrderBanner displayName={this.state.order.displayName} tableNumber={this.state.order.table} image={headerImg} status="Order" statusNumber={this.state.order.orders_id} />
+        {this.state.order.orderItems.map((line, i) => (
+          <Card
+            key={i}
+            className="order-line"
+            interactive={true}
+            elevation={Elevation.TWO}
+          >
+            <span className="order-item">{line.itemName}</span>
+          </Card>
+        ))}
+        <Card className="order-summary" elevation={Elevation.TWO}>
+          <img className="payment-method" src={paymentTest} alt="" />
+          <button className="payment-button">
+            <span className="payment-header">Pay Now</span>
+            <span className="payment-amount">&#36;360</span>
+          </button>
+        </Card>
 
-                <Usermenu />
-            </div>
-        );
-    }
+        <Usermenu />
+      </div>
+    );
+  }
 }
