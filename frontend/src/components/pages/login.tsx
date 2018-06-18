3be70266f93376c1d66b91a96a091aadb62ad995
ivanoung <ivanoung@gmail.com>
2018-06-18 22:03:45 +0800

    finished login page, 95%

--- frontend/src/components/pages/login.tsx
+++ frontend/src/components/pages/login.tsx
@@ -3,12 +3,12 @@ import * as History from "history";
 import * as React from "react";
 
 // Importing UI elements
-import { Card } from '@blueprintjs/core'
+import { Card } from "@blueprintjs/core";
 
 // Importing static assets
-import facebook from "../icons/facebookSignup.svg";
-import google from "../icons/googleSignup.svg";
-
+import facebook from "../icons/signup/facebook.svg";
+import google from "../icons/signup/google.svg";
+import logo from "../icons/all/logo.svg";
 
 interface ILoginProps {
   history: History.History;
@@ -27,36 +27,54 @@ export default class Login extends React.Component<ILoginProps> {
     return (
       <div className="login-container">
         <div className="login-top">
+          <div className="logo-container">
+            <img src={logo} alt="Dealing Room Logo" className="logo" />
+            <span className="logo-name">Dealing Room</span>
+          </div>
           <div className="social-login">
-            <div className="google">
-              <img src={google} alt="" />
+            <div className="banner rd-corner google">
+              <img className="banner-img" src={google} alt="" />
             </div>
-            <div className="facebook">
-              <img src={facebook} alt="" />
+            <div className="banner rd-corner facebook ">
+              <img className="banner-img" src={facebook} alt="" />
             </div>
+          </div>
+          <div className="divider">
+            <hr className="divider-break" />
+            <span className="divider-text">OR</span>
+            <hr className="divider-break" />
           </div>
         </div>
         <div className="login-bottom">
           <Card className="login-card rd-corner">
             <div className="status-switch">
               <div className="status">
-                <span className="status-text">
-                  LOGIN</span>
+                <span className="status-text">LOGIN</span>
               </div>
               <div className="status">
                 <span className="status-text">SIGNUP</span>
               </div>
             </div>
             <form className="form" action="">
-              <input className='form-input rd-corner' name="username" type="text" placeholder="Username" />
-              <input className='form-input rd-corner' placeholder="Passwords" name="username" type="text" />
+              <input
+                className="form-input rd-corner"
+                name="username"
+                type="text"
+                placeholder="Username"
+              />
+              <input
+                className="form-input rd-corner"
+                placeholder="Passwords"
+                name="username"
+                type="password"
+              />
             </form>
 
-            <div className="login-button "><button className="submit rd-corner">
-              <span className="submit-text">LOGIN</span>
-            </button>
+            <div className="login-button ">
+              <button className="submit rd-corner">
+                <span className="submit-text">LOGIN</span>
+              </button>
             </div>
-
           </Card>
         </div>
       </div>
