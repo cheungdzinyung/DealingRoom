73b0334982547a886145cedf8046a9ebce683a27
ivanoung <ivanoung@gmail.com>
2018-06-20 11:10:48 +0800

    new folder structure

--- frontend/src/components/ui/menuitem.tsx
+++ frontend/src/components/ui/menuitem.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 
 // importing UI elements
-import { Card, Elevation } from "@blueprintjs/core";
+import { Card, Elevation, Collapse } from "@blueprintjs/core";
+import {}
 
 // import static icons
 import wallet from "../icons/item/wallet.svg";
@@ -11,59 +12,64 @@ import up from "../icons/item/up.svg";
 import plus from "../icons/item/plus.svg";
 
 interface IMenuItemProps {
-  key: number;
-  itemName: string;
-  price: number;
-  priceDelta: number;
-  details: string;
-  image: "*.jpg" | "*.png" | "*.jpeg";
-  detailIsOpen: boolean
+    key: number;
+    itemName: string;
+    price: number;
+    priceDelta: number;
+    details: string;
+    image: "*.jpg" | "*.png" | "*.jpeg";
+    detailIsOpen: boolean
 }
 
 export default class MenuItem extends React.Component<IMenuItemProps> {
-  constructor(props: IMenuItemProps) {
-    super(props);
-  }
-  public render() {
-    return (
-      <div className="menu-item-container">
-        <Card
-          className="menu-item-card rd-corner"
-          interactive={true}
-          elevation={Elevation.ONE}
-          key={this.props.key}
-        >
-          {/* Absolute location */}
-          {/* Product images */}
-          <img src={this.props.image} className="menu-item-img" alt="" />
-          {/* Info button  */}
-          <img className="item-info" src={info} alt="" />
-          <img className="add-item" src={plus} alt="" />
-          <span className="menu-item-name">{this.props.itemName}</span>
-          <div className="menu-item-bot">
-            {/* Divider */}
-            <hr className="item-split" />
-            {/* Bottom part of the card */}
-            <div className="item-fluctuation">
-              <div className="item-flucutation-container">
-                <img className="flux-img" src={wallet} alt="" />
-                <span className="item-fluctuation-display">
-                  &#36;{this.props.price}
+    constructor(props: IMenuItemProps) {
+        super(props);
+    }
+    public render() {
+        return (
+            <div className="menu-item-container">
+                <Card
+                    className="menu-item-card rd-corner"
+                    interactive={true}
+                    elevation={Elevation.ONE}
+                    key={this.props.key}
+                >
+                    {/* Absolute location */}
+                    {/* Product images */}
+                    <img src={this.props.image} className="menu-item-img" alt="" />
+                    {/* Info button  */}
+                    <img className="item-info" src={info} alt="" />
+                    <img className="add-item" src={plus} alt="" />
+                    <span className="menu-item-name">{this.props.itemName}</span>
+                    <div className="menu-item-bot">
+                        {/* Divider */}
+                        <hr className="item-split" />
+                        {/* Bottom part of the card */}
+                        <div className="item-fluctuation">
+                            <div className="item-flucutation-container">
+                                <img className="flux-img" src={wallet} alt="" />
+                                <span className="item-fluctuation-display">
+                                    &#36;{this.props.price}
+                                </span>
+                            </div>
+                            <div className="item-flucutation-container">
+                                <img className="flux-img" src={up} alt="" />
+                                <span className="item-fluctuation-display">
+                                    {this.props.priceDelta}%
                 </span>
-              </div>
-              <div className="item-flucutation-container">
-                <img className="flux-img" src={up} alt="" />
-                <span className="item-fluctuation-display">
-                  {this.props.priceDelta}%
-                </span>
-              </div>
-            </div>
-          </div>
-        </Card>
-        {/* <Collapse className="item-detail-collapse" isOpen={this.props.detailIsOpen}>
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+                <Collapse className="item-detail-collapse" isOpen={this.props.detailIsOpen}>
+                    <h3>Details</h3>
+                    <hr className="item-split" />
+                    <p>{this.props.details}</p>
+                    <h3>Performance</h3>
+                    <hr className="item-split" />
 
-        </Collapse> */}
-      </div>
-    );
-  }
+                </Collapse>
+            </div>
+        );
+    }
 }
