6ce8a7b11605dbc6e8daac6e4d54f23795426940
ivanoung <ivanoung@gmail.com>
2018-06-21 23:42:01 +0800

    copied comments into respective folders

--- frontend/src/components/ui/desktop/stockitemline.tsx
+++ frontend/src/components/ui/desktop/stockitemline.tsx
@@ -38,3 +38,55 @@ export default class StockItemLine extends React.Component<IMenuItemWithoutFlux,
     );
   }
 }
+
+
+// public setCategory (e: React.MouseEvent<HTMLButtonElement>) {
+    //     this.setState ({
+    //         category: e.currentTarget.value
+    //     });
+    // }
+
+    // public setItemName (e: React.ChangeEvent<HTMLInputElement>) {
+    //     this.setState ({
+    //         itemName: e.target.value
+    //     });
+    // }
+
+    // public setItemDescription (e: React.ChangeEvent<HTMLInputElement>) {
+    //     this.setState ({
+    //         itemDescription: e.target.value
+    //     });
+    // }
+
+    // // need to parseFloat() when send to BE
+    // public setItemMinPrice (e: React.ChangeEvent<HTMLInputElement>) {
+    //     this.setState ({
+    //         itemDescription: e.target.value
+    //     });
+    // }
+
+    // // need to parseFloat() when send to BE
+    // public setItemStartPrice (e: React.ChangeEvent<HTMLInputElement>) {
+    //     this.setState ({
+    //         itemDescription: e.target.value
+    //     });
+    // }
+
+    // // need to parseInt() when send to BE
+    // public setItemQuantity (e: React.ChangeEvent<HTMLInputElement>) {
+    //     this.setState ({
+    //         itemDescription: e.target.value
+    //     });
+    // }
+
+    // public toggleActive () {
+    //     this.setState ({
+    //         isActive: !this.state.isActive
+    //     });
+    // }
+
+    // public toggleSpecial () {
+    //     this.setState ({
+    //         isSpecial: !this.state.isSpecial
+    //     });
+    // }
