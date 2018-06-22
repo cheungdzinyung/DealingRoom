76f299e8045fef24f0dfa47265533f57b224afae
ivanoung <ivanoung@gmail.com>
2018-06-23 00:32:39 +0800

    finishing up the manager stock item menu

--- frontend/src/components/container/desktop/stockmanagement.tsx
+++ frontend/src/components/container/desktop/stockmanagement.tsx
@@ -7,237 +7,240 @@ import StockFilter from "../../ui/desktop/stockfilter";
 import PageHeader from "../../ui/desktop/pageheader";
 
 // Importing interfaces
-import { ActiveSpecialFilter, IMenuCategoryWithoutFlux, ICreateMenuItem, IEditMenuItem } from "src/modules";
+import {
+  ActiveSpecialFilter,
+  IMenuCategoryWithoutFlux,
+  ICreateMenuItem,
+  IEditMenuItem
+} from "src/modules";
 
 // redux
 import { connect } from "react-redux";
 import { IRootState } from "../../../redux/store";
-import { getEntireMenu, createItem, changeItemStatus } from "../../../redux/desktop/actions/actions_manager";
+import {
+  getEntireMenu,
+  createItem,
+  changeItemStatus
+} from "../../../redux/desktop/actions/actions_manager";
 import StockItemLine from "../../ui/desktop/stockitemline";
 import { adminAllItemTest } from "../../../fakedata";
 
 interface IStockManagementProps {
-    menuReady: boolean,
-    entireMenu: IMenuCategoryWithoutFlux[],
-    categories: string[],
-    getEntireMenu: () => void,
-
-
-    // this goes to create page's state for new item input
-    createItem: (itemStatus: ICreateMenuItem) => void,
-
-    // this goes to edit page's state for item changes
-    changeItemStatus: (itemStatus: IEditMenuItem) => void,
+  menuReady: boolean;
+  entireMenu: IMenuCategoryWithoutFlux[];
+  categories: string[];
+  getEntireMenu: () => void;
+  // this goes to create page's state for new item input
+  createItem: (itemStatus: ICreateMenuItem) => void;
+  // this goes to edit page's state for item changes
+  changeItemStatus: (itemStatus: IEditMenuItem) => void;
 }
 
 interface IStockManagementState {
-    category: string
-    isActive: ActiveSpecialFilter
-    isSpecial: ActiveSpecialFilter
+  category: string;
+  isActive: ActiveSpecialFilter;
+  isSpecial: ActiveSpecialFilter;
 
-    // this goes to create page's state for new item input
-    // items_id: number;            // NO ENTRY !! gen by BE
-    // itemName: string;
-    // itemStock: number;
-    // categoryName: string;
-    // itemDescription: string;
-    // minimumPrice: number;
-    // currentPrice: number;        // starting price
-    // itemPhoto: any;
-    // isSpecial: boolean;
-    // isActive: boolean;
+  // this goes to create page's state for new item input
+  // items_id: number;            // NO ENTRY !! gen by BE
+  // itemName: string;
+  // itemStock: number;
+  // categoryName: string;
+  // itemDescription: string;
+  // minimumPrice: number;
+  // currentPrice: number;        // starting price
+  // itemPhoto: any;
+  // isSpecial: boolean;
+  // isActive: boolean;
 
-    // this goes to edit page's state for item changes
-    // items_id: number;            // NO MOD !!
-    // itemName: string;
-    // itemStock: number;
-    // categoryName: string;
-    // itemDescription: string;
-    // minimumPrice: number;
-    // currentPrice: number;        // NO MOD !!
-    // itemPhoto: any;
-    // isSpecial: boolean;
-    // isActive: boolean;
+  // this goes to edit page's state for item changes
+  // items_id: number;            // NO MOD !!
+  // itemName: string;
+  // itemStock: number;
+  // categoryName: string;
+  // itemDescription: string;
+  // minimumPrice: number;
+  // currentPrice: number;        // NO MOD !!
+  // itemPhoto: any;
+  // isSpecial: boolean;
+  // isActive: boolean;
 }
 
-export class PureStockManagement extends React.Component<IStockManagementProps, IStockManagementState> {
-    constructor(props: IStockManagementProps) {
-        super(props)
+export class PureStockManagement extends React.Component<
+  IStockManagementProps,
+  IStockManagementState
+> {
+  constructor(props: IStockManagementProps) {
+    super(props);
 
-        this.state = {
-            category: "all",
-            isActive: "all",
-            isSpecial: "all"
-        }
+    this.state = {
+      category: "all",
+      isActive: "all",
+      isSpecial: "all"
+    };
+  }
+
+  public filterByCategory(e: React.MouseEvent<HTMLButtonElement>) {
+    this.setState({
+      category: e.currentTarget.value
+    });
+  }
+
+  public filterByActive(e: React.MouseEvent<HTMLButtonElement>) {
+    let choice: ActiveSpecialFilter;
+    if (e.currentTarget.value === "not active") {
+      choice = false;
+    } else if (e.currentTarget.value === "active") {
+      choice = true;
+    } else {
+      choice = "all";
     }
+    this.setState({
+      isActive: choice
+    });
+  }
 
-    public filterByCategory(e: React.MouseEvent<HTMLButtonElement>) {
-        this.setState({
-            category: e.currentTarget.value
-        });
+  public filterBySpecial(e: React.MouseEvent<HTMLButtonElement>) {
+    let choice: ActiveSpecialFilter;
+    if (e.currentTarget.value === "not active") {
+      choice = false;
+    } else if (e.currentTarget.value === "active") {
+      choice = true;
+    } else {
+      choice = "all";
     }
+    this.setState({
+      isSpecial: choice
+    });
+  }
 
-    public filterByActive(e: React.MouseEvent<HTMLButtonElement>) {
-        let choice: ActiveSpecialFilter;
-        if (e.currentTarget.value === "not active") {
-            choice = false
-        } else if (e.currentTarget.value === "active") {
-            choice = true;
-        } else {
-            choice = "all";
-        }
-        this.setState({
-            isActive: choice
-        });
+  public goToAdd(e: React.MouseEvent<HTMLDivElement>) {
+    // trigger to open moddle or wtever page
+    // this.props.clickToAdd();
+  }
+
+  public goToEdit(e: React.MouseEvent<HTMLDivElement>) {
+    // trigger to open moddle or wtever page
+    // this.props.clickToEdit(this.props.item_id);
+  }
+
+  public componentDidMount() {
+    if (!this.props.menuReady) {
+      this.props.getEntireMenu();
     }
+  }
 
-    public filterBySpecial(e: React.MouseEvent<HTMLButtonElement>) {
-        let choice: ActiveSpecialFilter;
-        if (e.currentTarget.value === "not active") {
-            choice = false
-        } else if (e.currentTarget.value === "active") {
-            choice = true;
-        } else {
-            choice = "all";
-        }
-        this.setState({
-            isSpecial: choice
-        });
-    }
+  public render() {
+    return (
+      <div className="desktop-page-container">
+        <AdminSideMenu />
+        <div className="page-container-center">
+          <PageHeader header="Stock Management" />
 
+          {adminAllItemTest.map((eachItem, index) => (
+            <StockItemLine {...eachItem} />
+          ))}
+        </div>
+        <StockFilter />
+      </div>
+    );
+  }
 
-    public goToAdd(e: React.MouseEvent<HTMLDivElement>) {
-        // trigger to open moddle or wtever page
-        // this.props.clickToAdd();
-    }
+  // // in add / edit page
+  // public setCategory (e: React.MouseEvent<HTMLButtonElement>) {
+  //     this.setState ({
+  //         category: e.currentTarget.value
+  //     });
+  // }
 
-    public goToEdit(e: React.MouseEvent<HTMLDivElement>) {
-        // trigger to open moddle or wtever page
-        // this.props.clickToEdit(this.props.item_id);
-    }
+  // public setItemName (e: React.ChangeEvent<HTMLInputElement>) {
+  //     this.setState ({
+  //         itemName: e.target.value
+  //     });
+  // }
 
-    public componentDidMount() {
-        if (!this.props.menuReady) {
-            this.props.getEntireMenu();
-        }
-    }
+  // public setItemDescription (e: React.ChangeEvent<HTMLInputElement>) {
+  //     this.setState ({
+  //         itemDescription: e.target.value
+  //     });
+  // }
 
-    public render() {
-        return (
-            <div className="desktop-page-container">
-                <AdminSideMenu />
-                <div className="page-container-center">
-                    <PageHeader header="Stock Management" />
+  // // need to parseFloat() when send to BE
+  // public setItemMinPrice (e: React.ChangeEvent<HTMLInputElement>) {
+  //     this.setState ({
+  //         itemDescription: e.target.value
+  //     });
+  // }
 
-                    {
-                        adminAllItemTest.map((eachItem, index) => (
-                            <StockItemLine {...eachItem} />
-                        ))
-                    }
+  // // need to parseFloat() when send to BE
+  // public setItemStartPrice (e: React.ChangeEvent<HTMLInputElement>) {
+  //     this.setState ({
+  //         itemDescription: e.target.value
+  //     });
+  // }
 
-                </div>
-                <StockFilter />
-            </div>
-        );
-    }
+  // // need to parseInt() when send to BE
+  // public setItemQuantity (e: React.ChangeEvent<HTMLInputElement>) {
+  //     this.setState ({
+  //         itemDescription: e.target.value
+  //     });
+  // }
 
+  // public toggleActive () {
+  //     this.setState ({
+  //         isActive: !this.state.isActive
+  //     });
+  // }
 
-    // // in add / edit page
-    // public setCategory (e: React.MouseEvent<HTMLButtonElement>) {
-    //     this.setState ({
-    //         category: e.currentTarget.value
-    //     });
-    // }
+  // public toggleSpecial () {
+  //     this.setState ({
+  //         isSpecial: !this.state.isSpecial
+  //     });
+  // }
 
-    // public setItemName (e: React.ChangeEvent<HTMLInputElement>) {
-    //     this.setState ({
-    //         itemName: e.target.value
-    //     });
-    // }
-
-    // public setItemDescription (e: React.ChangeEvent<HTMLInputElement>) {
-    //     this.setState ({
-    //         itemDescription: e.target.value
-    //     });
-    // }
-
-    // // need to parseFloat() when send to BE
-    // public setItemMinPrice (e: React.ChangeEvent<HTMLInputElement>) {
-    //     this.setState ({
-    //         itemDescription: e.target.value
-    //     });
-    // }
-
-    // // need to parseFloat() when send to BE
-    // public setItemStartPrice (e: React.ChangeEvent<HTMLInputElement>) {
-    //     this.setState ({
-    //         itemDescription: e.target.value
-    //     });
-    // }
-
-    // // need to parseInt() when send to BE
-    // public setItemQuantity (e: React.ChangeEvent<HTMLInputElement>) {
-    //     this.setState ({
-    //         itemDescription: e.target.value
-    //     });
-    // }
-
-    // public toggleActive () {
-    //     this.setState ({
-    //         isActive: !this.state.isActive
-    //     });
-    // }
-
-    // public toggleSpecial () {
-    //     this.setState ({
-    //         isSpecial: !this.state.isSpecial
-    //     });
-    // }
-
-    // public confirmAdd() {
-    //     const newItem = {
-    //         // items_id: number;            // NO ENTRY !! gen by BE
-    //         itemName: this.state.itemName,
-    //         itemStock: this.state.itemStock,
-    //         categoryName: this.state.categoryName,
-    //         itemDescription: this.state.itemDescription,
-    //         minimumPrice: this.state.minimumPrice,
-    //         currentPrice: this.state.currentPrice,        // starting price
-    //         itemPhoto: this.state.itemPhoto,
-    //         isSpecial: this.state.isSpecial,
-    //         isActive: this.state.isActive,
-    //     };
-    //     this.props.createItem(newItem);
-    // }
-
-
-
+  // public confirmAdd() {
+  //     const newItem = {
+  //         // items_id: number;            // NO ENTRY !! gen by BE
+  //         itemName: this.state.itemName,
+  //         itemStock: this.state.itemStock,
+  //         categoryName: this.state.categoryName,
+  //         itemDescription: this.state.itemDescription,
+  //         minimumPrice: this.state.minimumPrice,
+  //         currentPrice: this.state.currentPrice,        // starting price
+  //         itemPhoto: this.state.itemPhoto,
+  //         isSpecial: this.state.isSpecial,
+  //         isActive: this.state.isActive,
+  //     };
+  //     this.props.createItem(newItem);
+  // }
 }
 
 // Redux
 const mapStateToProps = (state: IRootState) => {
-    return {
-        entireMenu: state.staff.manager.entireMenu,
-        categories: state.staff.manager.categories,
-        menuReady: state.staff.manager.menuReady,
-    }
-}
+  return {
+    entireMenu: state.staff.manager.entireMenu,
+    categories: state.staff.manager.categories,
+    menuReady: state.staff.manager.menuReady
+  };
+};
 
 const mapDispatchToProps = (dispatch: any) => {
-    return {
-        getEntireMenu: () => {
-            dispatch(getEntireMenu());
-        },
-        createItem: (itemStatus: ICreateMenuItem) => {
-            dispatch(createItem(itemStatus));
-        },
-        changeItemStatus: (itemStatus: IEditMenuItem) => {
-            dispatch(changeItemStatus(itemStatus));
-        },
-
+  return {
+    getEntireMenu: () => {
+      dispatch(getEntireMenu());
+    },
+    createItem: (itemStatus: ICreateMenuItem) => {
+      dispatch(createItem(itemStatus));
+    },
+    changeItemStatus: (itemStatus: IEditMenuItem) => {
+      dispatch(changeItemStatus(itemStatus));
     }
-}
+  };
+};
 
-const StockManagement = connect(mapStateToProps, mapDispatchToProps)(PureStockManagement);
+const StockManagement = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PureStockManagement);
 
-export default StockManagement
+export default StockManagement;
--- frontend/src/components/ui/desktop/stockitemline.tsx
+++ frontend/src/components/ui/desktop/stockitemline.tsx
@@ -5,38 +5,65 @@ import * as React from "react";
 import { IMenuItemWithoutFlux } from "src/modules";
 
 // Importing utility function
-import { firstLetterCaps } from '../../../util/utility';
+import { firstLetterCaps } from "../../../util/utility";
 
+// Importing static image assets
+import FilledStar from "../../assets/icons/desktop/stocklist/starfilled.svg";
+import UnfilledStar from "../../assets/icons/desktop/stocklist/starunfilled.svg";
+
+// Importing temp fake images
 import img from "../../assets/images/categories/squarebeer.jpg";
 
-export default class StockItemLine extends React.Component<IMenuItemWithoutFlux, {}> {
+export default class StockItemLine extends React.Component<
+  IMenuItemWithoutFlux,
+  {}
+> {
   constructor(props: IMenuItemWithoutFlux) {
     super(props);
   }
   public render() {
     return (
-      <div className="stock-item-card rd-corner" data-productId={this.props.items_id}>
+      <div
+        className="stock-item-card rd-corner"
+        data-productId={this.props.items_id}
+      >
         <img src={img} alt="" className="stock-item-img rd-corner" />
         <div className="stock-item-info">
-          <span className="stock-item-category">{firstLetterCaps(this.props.categoryName)}</span>
+          <span className="stock-item-category">
+            {firstLetterCaps(this.props.categoryName)}
+          </span>
           <div className="stock-item-name-price">
             <span className="stock-item-name">{this.props.itemName}</span>
-            <span className="stock-item-price">&#36;{this.props.currentPrice}</span>
+            <span className="stock-item-price">
+              &#36;{this.props.currentPrice}
+            </span>
           </div>
           <p className="stock-item-description">{this.props.itemDescription}</p>
           <div className="stock-item-price-floor">
             <span className="stock-item-price-floor-text">Price floor:</span>
-            <span className="stock-item-price-floor-number">&#36;{this.props.minimumPrice}</span>
+            <span className="stock-item-price-floor-number">
+              &#36;{this.props.minimumPrice}
+            </span>
           </div>
         </div>
         <div className="stock-item-mod">
           <div className="spec-act">
             <div className="isSpecial">
-              <img src="" alt="" className="star" />
+              {this.props.isSpecial ? (
+                <img src={FilledStar} alt="" className="star" />
+              ) : (
+                <img src={UnfilledStar} alt="" className="star" />
+              )}
             </div>
-            <div className="isActive">
-              <span>Active</span>
-            </div>
+            {this.props.isActive ? (
+              <button className="active-button rd-corner isActive">
+                <span className="isActive-button-text">Active</span>
+              </button>
+            ) : (
+              <button className="active-button rd-corner isNotActive">
+                <span className="isActive-button-text">Inactive</span>
+              </button>
+            )}
           </div>
         </div>
       </div>
@@ -44,54 +71,53 @@ export default class StockItemLine extends React.Component<IMenuItemWithoutFlux,
   }
 }
 
-
 // public setCategory (e: React.MouseEvent<HTMLButtonElement>) {
-    //     this.setState ({
-    //         category: e.currentTarget.value
-    //     });
-    // }
+//     this.setState ({
+//         category: e.currentTarget.value
+//     });
+// }
 
-    // public setItemName (e: React.ChangeEvent<HTMLInputElement>) {
-    //     this.setState ({
-    //         itemName: e.target.value
-    //     });
-    // }
+// public setItemName (e: React.ChangeEvent<HTMLInputElement>) {
+//     this.setState ({
+//         itemName: e.target.value
+//     });
+// }
 
-    // public setItemDescription (e: React.ChangeEvent<HTMLInputElement>) {
-    //     this.setState ({
-    //         itemDescription: e.target.value
-    //     });
-    // }
+// public setItemDescription (e: React.ChangeEvent<HTMLInputElement>) {
+//     this.setState ({
+//         itemDescription: e.target.value
+//     });
+// }
 
-    // // need to parseFloat() when send to BE
-    // public setItemMinPrice (e: React.ChangeEvent<HTMLInputElement>) {
-    //     this.setState ({
-    //         itemDescription: e.target.value
-    //     });
-    // }
+// // need to parseFloat() when send to BE
+// public setItemMinPrice (e: React.ChangeEvent<HTMLInputElement>) {
+//     this.setState ({
+//         itemDescription: e.target.value
+//     });
+// }
 
-    // // need to parseFloat() when send to BE
-    // public setItemStartPrice (e: React.ChangeEvent<HTMLInputElement>) {
-    //     this.setState ({
-    //         itemDescription: e.target.value
-    //     });
-    // }
+// // need to parseFloat() when send to BE
+// public setItemStartPrice (e: React.ChangeEvent<HTMLInputElement>) {
+//     this.setState ({
+//         itemDescription: e.target.value
+//     });
+// }
 
-    // // need to parseInt() when send to BE
-    // public setItemQuantity (e: React.ChangeEvent<HTMLInputElement>) {
-    //     this.setState ({
-    //         itemDescription: e.target.value
-    //     });
-    // }
+// // need to parseInt() when send to BE
+// public setItemQuantity (e: React.ChangeEvent<HTMLInputElement>) {
+//     this.setState ({
+//         itemDescription: e.target.value
+//     });
+// }
 
-    // public toggleActive () {
-    //     this.setState ({
-    //         isActive: !this.state.isActive
-    //     });
-    // }
+// public toggleActive () {
+//     this.setState ({
+//         isActive: !this.state.isActive
+//     });
+// }
 
-    // public toggleSpecial () {
-    //     this.setState ({
-    //         isSpecial: !this.state.isSpecial
-    //     });
-    // }
+// public toggleSpecial () {
+//     this.setState ({
+//         isSpecial: !this.state.isSpecial
+//     });
+// }
