7d47c8ce9c038f169205b3d6bc6b67426e39f533
judith curtit <judithcurtit8@gmail.com>
2018-06-22 18:25:04 +0800

    api server change

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -18,8 +18,8 @@ export const rootReducer = combineReducers<IRootState>({
 
 
 // env
-// export const API_SERVER = process.env.REACT_APP_API_SERVER;
-export const API_SERVER = process.env.REACT_APP_API_DEV;
+export const API_SERVER = process.env.REACT_APP_API_SERVER;
+// export const API_SERVER = process.env.REACT_APP_API_DEV;
 
 
 // socket.io
