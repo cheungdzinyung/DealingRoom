db96305e71c6e32a0e32ed2d87982637ba1f921f
ivanoung <ivanoung@gmail.com>
2018-06-25 16:45:39 +0800

    catagories in manager add item menu ok

--- frontend/src/components/ui/desktop/stockitem/additemmodal/itemStatus.tsx
+++ frontend/src/components/ui/desktop/stockitem/additemmodal/itemStatus.tsx
@@ -1,18 +1,38 @@
 // Importing modules
 import * as React from "react";
 
-// interface IItemModalStatusProps {
+// Import util function
+import { firstLetterCaps } from "src/util/utility";
 
-// }
+export interface ItemModalStatusProps {
+  categories: string[];
+  setCategories: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
 
-export default class ItemModalStatus extends React.Component {
-  // constructor(props: IItemModalStatusProps) {
-  //   super(props);
-  // }
+
+export default class ItemModalStatus extends React.Component<ItemModalStatusProps> {
+  constructor(props: ItemModalStatusProps) {
+    super(props);
+  }
+
 
   public render() {
     return (
-      <div className="edit-item-status">hi</div>
+      <div className="edit-item-status">
+        <select
+          className="filter-select rd-corner"
+          defaultValue="beer"
+          onChange={this.props.setCategories}
+        >
+          {
+            this.props.categories.map((category, index) => (
+              <option key={index} value={category}>{firstLetterCaps(category)}</option>
+            ))
+          }
+        </select>
+
+      </div>
     );
   }
 }
+
