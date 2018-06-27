c4bfd1c6c4717a2b2933ea89333fca54b405689e
ivanoung <ivanoung@gmail.com>
2018-06-28 01:14:52 +0800

    done add item card styling

--- frontend/src/components/ui/desktop/stockitem/additemmodal/itemStatus.tsx
+++ frontend/src/components/ui/desktop/stockitem/additemmodal/itemStatus.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 
 // Importing static asset
 import solidStar from "src/components/assets/icons/desktop/stocklist/starfilled.svg";
-// import hollowStar from "src/components/assets/icons/desktop/stocklist/starunfilled.svg";
+import hollowStar from "src/components/assets/icons/desktop/stocklist/starunfilled.svg";
 // Import util function
 import { firstLetterCaps } from "src/util/utility";
 
@@ -16,12 +16,12 @@ export interface ItemModalStatusProps {
   toggleSpecial: () => void;
 }
 
-
-export default class ItemModalStatus extends React.Component<ItemModalStatusProps> {
+export default class ItemModalStatus extends React.Component<
+  ItemModalStatusProps
+> {
   constructor(props: ItemModalStatusProps) {
     super(props);
   }
-
 
   public render() {
     return (
@@ -31,20 +31,29 @@ export default class ItemModalStatus extends React.Component<ItemModalStatusProp
           defaultValue="beer"
           onChange={this.props.setCategories}
         >
-          {
-            this.props.categories.map((category, index) => (
-              <option key={index} value={category}>
-                {firstLetterCaps(category)}
-              </option>
-            ))
-          }
+          {this.props.categories.map((category, index) => (
+            <option key={index} value={category}>
+              {firstLetterCaps(category)}
+            </option>
+          ))}
         </select>
-        <div onClick={this.props.toggleSpecial} className="edit-item-star-container">
-          <img src={solidStar} alt="" className="edit-item-star-img" />
+        <div
+          onClick={this.props.toggleSpecial}
+          className="edit-item-star-container"
+        >
+          <img
+            src={this.props.isSpecial === true ? solidStar : hollowStar}
+            alt=""
+            className="edit-item-star-img"
+          />
         </div>
-        <button onClick={this.props.toggleActive} className="edit-item-status-switch">{this.props.isActive}</button>
+        <button
+          onClick={this.props.toggleActive}
+          className="edit-item-status-switch"
+        >
+          {this.props.isActive === true ? "Active" : "Inactive"}
+        </button>
       </div>
     );
   }
 }
-
