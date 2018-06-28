c70066a25f9ba0f8fd1bf5e737638b0ec4ee0370
ivanoung <ivanoung@gmail.com>
2018-06-28 17:13:29 +0800

    commit before play

--- frontend/src/components/ui/display/displayfluxitemline.tsx
+++ frontend/src/components/ui/display/displayfluxitemline.tsx
@@ -2,8 +2,8 @@
 import * as React from "react";
 
 // Importing static assets
-import upArrow from "src/components/assets/icons/item/up.svg";
-import downArrow from "src/components/assets/icons/item/down.svg";
+// import upArrow from "src/components/assets/icons/item/up.svg";
+// import downArrow from "src/components/assets/icons/item/down.svg";
 
 import { ResponsiveContainer, LineChart, Line } from "recharts";
 
@@ -36,11 +36,11 @@ export class DisplayFlexItemLine extends React.Component<
 
     return (
       <div className="display-data-prices-flux-line">
-        {this.state.delta < 0 ? (
+        {/* {this.state.delta < 0 ? (
           <img src={downArrow} alt="" className="display-data-arrow" />
         ) : (
             <img src={upArrow} alt="" className="display-data-arrow" />
-          )}
+          )} */}
         <span className="display-data-prices-flux-line-item-name">
           {this.props.itemName}
         </span>
@@ -50,14 +50,17 @@ export class DisplayFlexItemLine extends React.Component<
               <Line
                 type="monotone"
                 dataKey="purchasePrice"
-                stroke={this.state.delta < 0 ? "#F46868" : "#73EDA6"}
+                stroke={this.state.delta > 0 ? "#73EDA6" : "#F46868"}
                 strokeWidth={1}
                 dot={false}
               />
             </LineChart>
           </ResponsiveContainer>
         </div>
-        <span className="display-data-prices-flux-line-price">&#36;{this.props.currentPrice}</span>
+        <span className={
+          this.state.delta > 0 ? "display-data-prices-flux-line-price increase-price" :
+            "display-data-prices-flux-line-price decrease-price"
+        }>&#36;{this.props.currentPrice}</span>
       </div>
     );
   }
