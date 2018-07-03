16e8b7448e1764029e442884508d58f98147ac75
harrixon <harrisonchan1991@gmail.com>
2018-07-03 15:51:36 +0800

    merge new UI

--- frontend/src/Page/AdminUnpaidOrders/AdminUnpaidOrders.tsx
+++ frontend/src/Page/AdminUnpaidOrders/AdminUnpaidOrders.tsx
@@ -52,54 +52,88 @@ class PureUnpaidOrders extends React.Component<IUnpaidOrdersProps, IUnpaidOrders
         super(props);
 
         this.state = {
-            allOrders: {},
+            allOrders: [{
+                "orders_id": 0,
+                "users_id": 0,
+                "displayName": "nobody",
+                "table": 0,
+                "status": "made",
+                "isPaid": false,
+                "order": [{
+                    "itemName": "Corona",
+                    "ice": "normal",
+                    "sweetness": "normal",
+                    "garnish": "normal",
+                    "purchasePrice": "60.00"
+                }, {
+                    "itemName": "Asahi",
+                    "ice": "normal",
+                    "sweetness": "normal",
+                    "garnish": "normal",
+                    "purchasePrice": "60.00"
+                }]
+            }],
         }
     }
 
-    public componentWillMount() {
-        // TODO: check user role, redir if un-auth
-    }
+    // public componentWillMount() {
+    //     // TODO: check user role, redir if un-auth
+    // }
 
     public componentDidMount() {
         this.props.getAllOrders();
     }
 
-    public componentDidUpdate() {
-        this.setState({
-            allOrders: this.props.allOrders
-        })
-    }
+    // public componentDidUpdate() {
+    //     this.setState({
+    //         allOrders: this.props.allOrders
+    //     })
+    // }
 
-    // public onSortEnd = ({ oldIndex, newIndex }:{ oldIndex:number, newIndex: number}) => {
+    // public onSortEnd = ({ oldIndex, newIndex }: { oldIndex: number, newIndex: number }) => {
     //     this.setState({
     //         allOrders: arrayMove(this.state.allOrders, oldIndex, newIndex),
     //     });
     // };
 
     public render() {
-        // const SortableItem = SortableElement(({ oneOrder, index }) =>
-        //     <li>
-        //         <OrderCard
-        //             {...oneOrder}
-        //             paymentPage={true}
-        //             key={index} />
-        //     </li>
-        // )
+        // const SortableItem = SortableElement(({ index, ...oneOrder }) => {
+        //     // alert(JSON.stringify(oneOrder));
+        //     return (
+                
+        //             <OrderCard
+        //                 // {...oneOrder}
+        //                 orders_id={oneOrder.orders_id}
+        //                 users_id={oneOrder.users_id}
+        //                 displayName={oneOrder.displayName}
+        //                 table={oneOrder.table}
+        //                 status={oneOrder.status}
+        //                 isPaid={oneOrder.isPaid}
+        //                 order={oneOrder.order}
+        //                 paymentPage={true}
+        //                 key={index}
+        //             />
+                
+        //     )
+        // })
 
         // const SortableList = SortableContainer(() => {
         //     return (
         //         <ul>
         //             {this.state.allOrders
         //                 .filter((each: any) => each.isPaid === false)
-        //                 .map((oneOrder: any, index: number) => (
-        //                     <SortableItem
-        //                         key={`item-${index}`}
-        //                         index={index}
-        //                         {...oneOrder}
-        //                     // sortIndex={index}
-        //                     // value={value}
-        //                     />
-        //                 ))}
+        //                 .map((oneOrder: any, index: number) => {
+        //                     // alert(JSON.stringify(oneOrder))
+        //                     return (
+        //                         <SortableItem
+        //                             key={`item-${index}`}
+        //                             index={index}
+        //                             {...oneOrder}
+        //                             sortIndex={index}
+        //                         />
+        //                     )
+        //                 })
+        //             }
         //         </ul>
         //     );
         // })
