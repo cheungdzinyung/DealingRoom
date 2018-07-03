4c3649d14d2c9a6dd30a37027fe72f97b535554a
ivanoung <ivanoung@gmail.com>
2018-07-03 10:20:05 +0800

    omg structure

--- frontend/src/components/ui/mobile/toast.tsx
+++
@@ -1,7 +0,0 @@
-import { Position, Toaster } from "@blueprintjs/core";
-
-/** Singleton toaster instance. Create separate instances for different options. */
-export const AppToaster = Toaster.create({
-    className: "message-toaster",
-    position: Position.TOP,
-});
