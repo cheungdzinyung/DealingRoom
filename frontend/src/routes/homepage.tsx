eddf85d042d62236d6ec56dd988ef20d8e678aae
ivanoung <ivanoung@gmail.com>
2018-07-04 11:52:46 +0800

    changed the route back to template

--- frontend/src/routes/homepage.tsx
+++ frontend/src/routes/homepage.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 import { Switch, Route } from "react-router-dom";
 
 // import LandingPage from "src/Page/LandingPage/LandingPage";
-import LandingPageNew from "src/Page/LandingPageNew/LandingPageNew";
+import LandingPage from "src/Page/LandingPage/LandingPage";
 
 export default class HomepageRoutes extends React.Component {
   public render() {
     return (
       <Switch>
-        <Route path="/" exact={true} component={LandingPageNew} />
+        <Route path="/" exact={true} component={LandingPage} />
       </Switch>
     );
   }
