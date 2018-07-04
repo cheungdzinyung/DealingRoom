778c3dd798948941fab241ccf1550fb776c57072
ivanoung <ivanoung@gmail.com>
2018-07-05 00:38:11 +0800

    reroute landing page to mobile login

--- frontend/src/routes/homepage.tsx
+++ frontend/src/routes/homepage.tsx
@@ -3,13 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 // import LandingPage from "src/Page/LandingPage/LandingPage";
 // import LandingPage from "src/Page/LandingPage/LandingPage";
-import LandingPageNew from "src/Page/LandingPageNew/LandingPageNew"
+// import LandingPageNew from "src/Page/LandingPageNew/LandingPageNew";
+import Login from "src/Page/CustomerLogin/CustomerLogin";
 
 export default class HomepageRoutes extends React.Component {
   public render() {
     return (
       <Switch>
-        <Route path="/" exact={true} component={LandingPageNew} />
+        <Route path="/" exact={true} component={Login} />
       </Switch>
     );
   }
