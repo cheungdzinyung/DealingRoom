ad9a5b791cffbf1d0cac9f1d8351a5180a2ab3a9
ivanoung <ivanoung@gmail.com>
2018-07-08 00:44:49 +0800

    added GA for testing reactions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
 import "./scss/App.scss";
+import * as ReactGA from "react-ga";
 
 // Importing desktop containers
 import MobileRoutes from "src/routes/mobile";
 import DisplayRoutes from "src/routes/display";
 import DesktopRoutes from "src/routes/desktop";
 import HomepageRoutes from "src/routes/homepage";
-
 export default class App extends React.Component {
   public render() {
+    ReactGA.initialize("UA-74343046-8");
+    ReactGA.pageview(window.location.pathname + window.location.search);
     return (
       <div className="full-page">
         <HomepageRoutes />
